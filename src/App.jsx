17b0cde8347b29a7f2f2d9f4faac88d54189fa4d
skardam <sushikardam@gmail.com>
2025-03-04 17:11:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="text-lg text-center font-bold text-green-600">
+        <h1>Starting netflix-gpt</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
